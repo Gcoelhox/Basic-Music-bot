b10b304b5fc7e16c7feea72248d489c54e0718a5
Gcoelhox <67611982+Gcoelhox@users.noreply.github.com>
2020-06-29 20:59:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Declaração das chaves de acesso para as API's necessárias
 // Declaradas em arquivo separado para agilizar e deixar o código mais organizado
-exports.dToken = 'NzIzMDU4NDU2Mzc0MDE4MDk5.XusGvg.zUXs6CHyBsddZMy1JMgY9Sskuic';
-exports.gToken = 'AIzaSyAD7JYu3fkV2G8teHcBtETa_63vQQs3QY0';
+exports.dToken = '';
+exports.gToken = '';
